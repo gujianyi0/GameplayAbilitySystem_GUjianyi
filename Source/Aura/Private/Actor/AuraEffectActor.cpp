45ce6522b928816d5ff93cd0bc1d80dc91909fa5
gujianyi <c2115259095@163.com>
2025-07-22 00:42:23 +0800

    无限的游戏效果 未在蓝图中添加逻辑

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -34,3 +34,11 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	
 }
 
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+}
+
