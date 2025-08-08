1149756fb6c82e464c32d49a9dd8cfbdea82d01f
gujianyi <c2115259095@163.com>
2025-08-09 01:18:04 +0800

    使用Gameplay效果初始化属性（Initialize Attributes with Gameplay Effects）

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -65,4 +65,5 @@ void AAuraCharacter::InitAbilityActorInfo()
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		}
 	}
+	InitializePrimaryAttributes();
 }
