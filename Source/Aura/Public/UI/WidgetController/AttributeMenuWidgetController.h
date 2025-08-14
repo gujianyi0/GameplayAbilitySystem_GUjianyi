e3f3db5adda21cac8ed0f2d1841e528010ee2415
gujianyi <c2115259095@163.com>
2025-08-14 18:10:13 +0800

    属性信息委托

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,6 +6,10 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignatrue, const FAuraAttributeInfo&, Info);
+
 /**
  * 
  */
@@ -16,4 +20,12 @@ class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 public:
 	virtual void BindCallbacksToDependencies() override;
 	virtual void BroadcastInitialValues() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|AttributeMenu")
+	FAttributeInfoSignatrue AttributeInfoDelegate;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
