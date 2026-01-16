52d95beb4d6f57bca12f75b0cc7471e48e8fcd1d
gujianyi <c2115259095@163.com>
2026-01-17 02:16:55 +0800

    由调用者设置的幅值

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -119,6 +119,11 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("InputTag.4"),
 		FString("Input Tag for Keyboard 4")
 		);
+	
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Damage"),
+	FString("Damage")
+	);
 }
 
 
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -43,6 +43,8 @@ public:
 	FGameplayTag InputTag_3;
 	FGameplayTag InputTag_4;
 	
+	FGameplayTag Damage;
+	
 protected:
 
 private:
