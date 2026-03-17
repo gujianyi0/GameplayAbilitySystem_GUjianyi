8ef5a5de5ed4791498e59e7b92d4d02ce2bbddb6
gujianyi <c2115259095@163.com>
2026-03-17 12:13:03 +0800

    处理AI敌人转向以及拾取物品的问题

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -9,6 +9,7 @@
 
 AAuraEffectActor::AAuraEffectActor()
 {
+	// 设置当前对象是否每帧调用Tick()
 	PrimaryActorTick.bCanEverTick = false;
 
 	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
@@ -21,25 +22,38 @@ void AAuraEffectActor::BeginPlay()
 
 void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
+	if (TargetActor->ActorHasTag(FName("Enemy")) && !bApplyEffectsToEnemies) return;
+	//获取ASC
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
-	
 	if (TargetASC == nullptr) return;
 
 	check(GameplayEffectClass);
+	//创建Effect的句柄 包含了实例化Effect所需数据
 	FGameplayEffectContextHandle EffectContextHandle =TargetASC->MakeEffectContext();
+	//设置创建Effect的对象
 	EffectContextHandle.AddSourceObject(this);
+	//Effect的实例化后的句柄，可以通过此来寻找调用
 	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, ActorLevel, EffectContextHandle);
+	//从句柄中获取到实例的地址，并被应用
 	const FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 	
+	//从句柄中获取到定义的对象，并判断设置的
 	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	//在是无限时间效果和需要在结束时清除掉时，将效果句柄添加到map
 	if (bIsInfinite && InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
 	{
 		ActiveEffectHandles.Add(ActiveEffectHandle, TargetASC);
+	}
+	if (!bIsInfinite)//如果设置了应用时删除，除了Infinite的都会自动删除
+	{
+		Destroy();
 	}
 }
 
 void AAuraEffectActor::OnOverlap(AActor* TargetActor)
 {
+	//如果触发角色类型为敌人，并且此拾取物设置无法被敌人拾取
+	if (TargetActor->ActorHasTag(FName("Enemy")) && !bApplyEffectsToEnemies) return;
 	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
 	{
 		ApplyEffectToTarget(TargetActor,InstantGameplayEffectClass);
@@ -58,6 +72,10 @@ void AAuraEffectActor::OnOverlap(AActor* TargetActor)
 
 void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
 {
+	//如果触发角色类型为敌人，并且此拾取物设置无法被敌人拾取
+	if (TargetActor->ActorHasTag(FName("Enemy")) && !bApplyEffectsToEnemies) return;
+	
+	//添加效果
 	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
 	{
 		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
@@ -70,20 +88,28 @@ void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
 	{
 		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
 	}
+	
+	//删除效果
 	if (InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
 	{
 		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 		if (!IsValid(TargetASC)) return;
 
+		//创建存储需要移除的效果句柄存储Key，用于遍历完成后移除效果
 		TArray<FActiveGameplayEffectHandle>HandlesToRemove;
+		//循环map内存的数据
 		for (TTuple<FActiveGameplayEffectHandle, UAbilitySystemComponent*> HandlePair : ActiveEffectHandles)
 		{
+			//判断是否ASC相同
 			if (TargetASC == HandlePair.Value)
 			{
+				//通过句柄将效果移除，注意，有可能有多层效果，不能将其它层的效果也移除掉，所以只移除一层
 				TargetASC->RemoveActiveGameplayEffect(HandlePair.Key,1);
+				//添加到移除列表
 				HandlesToRemove.Add(HandlePair.Key);
 			}
 		}
+		//遍历完成后，在Map中将移除效果的KeyValue删除
 		for (FActiveGameplayEffectHandle& Handle : HandlesToRemove)
 		{
 			ActiveEffectHandles.FindAndRemoveChecked(Handle);
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -13,6 +13,7 @@ class UAbilitySystemComponent;
 class UGameplayEffect;
 class USphereComponent;
 
+//效果应用状态枚举
 UENUM(BlueprintType)
 enum class EEffectApplicationPolicy: uint8
 {
@@ -21,6 +22,7 @@ enum class EEffectApplicationPolicy: uint8
 	DoNotApply
 };
 
+//效果移除的状态枚举
 UENUM(BlueprintType)
 enum class EEffectRemovalPolicy: uint8
 {
@@ -28,6 +30,9 @@ enum class EEffectRemovalPolicy: uint8
 	DoNotRemove
 };
 
+/**
+ * 在场景中可放置的影响角色属性的物件基类
+ */
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
 {
@@ -37,20 +42,28 @@ public:
 	AAuraEffectActor();
 
 protected:
-	
+	// 游戏开始或生成对象时回调
 	virtual void BeginPlay() override;
 
+	//给与目标添加GameplayEffect效果
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);//给与目标添加GameplayEffect
 
+	//在重叠结束时处理效果的添加删除逻辑
 	UFUNCTION(BlueprintCallable)
 	void OnOverlap(AActor* TargetActor);
 
+	
 	UFUNCTION(BlueprintCallable)
 	void OnEndOverlap(AActor* TargetActor);
 
+	//Instant和Duration的GE在应用后，此物体是否需要被销毁
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	bool bDestroyOnEffectRemoval = false;
+	bool bDestroyOnEffectApplication = false;
+
+	//敌人是否能够拾取此物体
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	bool bApplyEffectsToEnemies = false;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
@@ -59,13 +72,13 @@ protected:
 	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;//生成具有一定持续时间的GameplayEffect的类
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;//生成具有一定持续时间的GameplayEffect的类
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	EEffectApplicationPolicy InfiniteEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
@@ -73,6 +86,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
 	
+	//用于存储当前已经激活的GameplayEffect的句柄的map
 	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
