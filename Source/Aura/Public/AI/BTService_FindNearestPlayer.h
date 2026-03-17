c76d4a3a5ec56028d188766d27020eaa688b9142
gujianyi <c2115259095@163.com>
2026-03-17 10:25:14 +0800

    在黑板添加属性

--- Source/Aura/Public/AI/BTService_FindNearestPlayer.h
+++ Source/Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -18,4 +18,10 @@ protected:
 
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector DistanceToTargetSelector;
+	
 };
