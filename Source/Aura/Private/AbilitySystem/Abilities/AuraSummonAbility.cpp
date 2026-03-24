897270ab31368a2729616a5e0d0229a10182e0b4
gujianyi <c2115259095@163.com>
2026-03-24 18:42:32 +0800

    异步生成时间

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -3,9 +3,6 @@
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
 
-#include "NiagaraBakerSettings.h"
-#include "Kismet/KismetSystemLibrary.h"
-
 
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
@@ -18,14 +15,17 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 	TArray<FVector> SpawnLocations;
 	for (int32 i = 0; i < NumMinions; i++)//遍历，在每个分段上面获取位置
 	{
-		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);//获取当前分段的角度
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);//随机位置，加上原始位置就是偏移的位置
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);//获取当前分段的角度 
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);//随机位置，加上原始位置就是偏移的位置
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f, 0.f, 400.f), ChosenSpawnLocation - FVector(0.f, 0.f, 400.f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
 		SpawnLocations.Add(ChosenSpawnLocation);
 		
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f );
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Green, 3.f );
-		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
-		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
 	}
 	
 	return SpawnLocations;
