41364dd94c705373e88c29d15ae947e3d0a414d5
gujianyi <c2115259095@163.com>
2026-04-14 08:27:13 +0800

    显示法术点数

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -4,11 +4,13 @@
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/Data/AbilityInfo.h"
-
+#include "Player/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
 	BroadcastAbilityInfo();
+	
+	SpellPointsChanged.Broadcast(GetAuraPS()->GetSpellPoints());
 }
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
@@ -22,4 +24,10 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 		AbilityInfoDelegate.Broadcast(Info);
 	}
 });
+	
+	//绑定技能点变动回调
+	GetAuraPS()->OnSpellPointsChangedDelegate.AddLambda([this](int32 SpellPoints)
+	{
+		SpellPointsChanged.Broadcast(SpellPoints); //广播拥有的技能点
+	});
 }
