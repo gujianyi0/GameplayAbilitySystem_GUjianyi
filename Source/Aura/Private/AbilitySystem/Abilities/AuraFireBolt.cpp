f18ef7fdda7f6e714cd96b6e3cf53fb3f596b6a7
gujianyi <c2115259095@163.com>
2026-04-21 02:57:13 +0800

    多段火球

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -3,6 +3,7 @@
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
 
+#include "Kismet/KismetSystemLibrary.h"
 #include "Aura/Public/AuraGameplayTags.h"
 
 FString UAuraFireBolt::GetDescription(int32 Level)
@@ -94,4 +95,61 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 	FMath::Min(Level, NumProjectiles),
 	ScaledDamage);
 		
+}
+
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();//判断此函数是否在服务器运行
+	if (!bIsServer) return;
+
+	//获取释放位置
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(),SocketTag);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();//将方向转为旋转
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;//覆写发射角度
+	
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);//获取到最左侧的角度
+	const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);//获取到最右侧的角度
+	
+
+	//限制产生火球的最大数量
+	//NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	
+	//根据可生成数量进行逻辑判断
+	if (NumProjectiles > 1)
+	{
+		
+		const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1);//技能分的段数
+		for (int32 i = 0; i < NumProjectiles; i++)
+		{
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			const FVector Start = SocketLocation + FVector(0,0,5);
+			UKismetSystemLibrary::DrawDebugArrow(
+				GetAvatarActorFromActorInfo(),
+				Start,
+				Start + Direction * 75.f,
+				1,
+				FLinearColor::Red,
+				120,
+				1);
+		}
+	}
+	else
+	{
+		// Single projectile
+		const FVector Start = SocketLocation + FVector(0,0,5);
+		UKismetSystemLibrary::DrawDebugArrow(
+				GetAvatarActorFromActorInfo(),
+				Start,
+				Start + Forward * 75.f,
+				1,
+				FLinearColor::Red,
+				120,
+				1);
+	}
+
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 100.f, 1, FLinearColor::White, 120, 1);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.f, 1, FLinearColor::Gray, 120, 1);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.f, 1, FLinearColor::Gray, 120, 1);
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -17,4 +17,18 @@ class AURA_API UAuraFireBolt : public UAuraProjectileSpell
 public:
 	virtual FString GetDescription(int32 Level) override;//获取投射技能描述
 	virtual FString GetNextLevelDescription(int32 Level) override;//获取投射技能下一等级描述
+	
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+
+protected:
+
+	//攻击角度
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	float ProjectileSpread = 90.f;
+
+	//最大生成火球数量
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	int32 MaxNumProjectiles = 5;	
+	
 };
