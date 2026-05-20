ed3ba73bab1d8e74616275fc580774d22287d644
gujianyi <c2115259095@163.com>
2026-05-20 21:15:32 +0800

    造成火球伤害

--- Source/Aura/Private/Actor/AuraFireBall.cpp
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -2,7 +2,8 @@
 
 
 #include "Actor/AuraFireBall.h"
-
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 
 void AAuraFireBall::BeginPlay()
 {
@@ -12,5 +13,20 @@ void AAuraFireBall::BeginPlay()
 
 void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!IsValidOverlap(OtherActor)) return;//判断和碰撞体接触的目标是否为自身
 	
+	//在重叠后，销毁自身
+	if (HasAuthority())//是否为服务器，如果服务器端，将执行应用GE，并通过GE的自动同步到客户端
+	{
+		//为目标应用GE
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			//死亡冲击的力度和方向
+			const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.DeathImpulse = DeathImpulse;
+			//通过配置项应用给目标ASC
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
+			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+		}
+	}
 }
